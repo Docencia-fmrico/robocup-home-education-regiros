910c6dc6011897234fbc34e7a4eaa051c74219d4
Daniel Alejandro Quinga Lopez <da.quinga.2020@alumnos.urjc.es>
2022-05-06 11:25:24 +0200

    DetectedSeat done

--- recepcionist/include/recepcionist/DetectSeat.h
+++ recepcionist/include/recepcionist/DetectSeat.h
@@ -17,8 +17,19 @@
 
 #include "behaviortree_cpp_v3/behavior_tree.h"
 #include "behaviortree_cpp_v3/bt_factory.h"
+#include "darknet_ros_msgs/BoundingBoxes.h"
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "sensor_msgs/CameraInfo.h"
+#include "geometry_msgs/Twist.h"
+
+#include <iostream>
+#include <chrono>
+
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
 #include "string"
 
 namespace recepcionist
@@ -28,14 +39,29 @@ class DetectSeat : public BT::ActionNodeBase
   public:
     explicit DetectSeat(const std::string& name);
 
+    void callback_chair(const sensor_msgs::CameraInfoConstPtr& cinf, const darknet_ros_msgs::BoundingBoxesConstPtr& boxes);
+
     void halt();
 
     BT::NodeStatus tick();
 
   private:
     ros::NodeHandle nh_;
-    int count_;
-};
-};  // namespace recepcionist
+    message_filters::Subscriber<sensor_msgs::CameraInfo> cinf_sub_;
+    message_filters::Subscriber<darknet_ros_msgs::BoundingBoxes> bbx_sub_;
 
-#endif  // RECEPCIONIST_DETECTSEAT
+    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::CameraInfo, darknet_ros_msgs::BoundingBoxes> MySyncPolicy_bbx;
+    message_filters::Synchronizer<MySyncPolicy_bbx> sync_bbx_;
+    
+    geometry_msgs::Twist twist;
+    ros::Publisher pub_;
+    int count_;
+    bool detected_;
+    bool positioned_;
+    int dir_;
+    bool firsttick_;
+    ros::Time initTime_;
+};
+};  // namespace speech
+
+#endif  // SPEECH_DETECTSEAT_H
